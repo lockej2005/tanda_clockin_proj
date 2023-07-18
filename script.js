bbdc17907a39fa50ada674201b1b1f83afe97529
lockej2005 <josh@intelligentinstallations.com.au>
2023-07-19 03:01:34 +1000

    applying eslint

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // eslint-disable-next-line no-unused-vars, no-undef
   const app = new Vue({
     el: '#app',
-    data: {
-      shifts: [],
+    data: function () {
+      return {
+        shifts: []
+      }
     },
     created: function () {
       const fetchJSON = () => {
@@ -11,228 +14,229 @@ document.addEventListener('DOMContentLoaded', function () {
           .then((data) => {
             if (data && Array.isArray(data.shifts)) {
               data.shifts.forEach((shift) => {
-                let [dayName, date, month, year] = shift.date.split(' ');
-                date = date.replace(/(st|nd|rd|th)/, '');
-                let [time, period] = shift.clock_in.split(/\s/);
-                let [hour, minute, seconds] = time.split(':');
-                hour = parseInt(hour);
-                minute = parseInt(minute);
-                seconds = parseInt(seconds);
+                let [date, month, year] = shift.date.split(' ')
+                date = date.replace(/(st|nd|rd|th)/, '')
+                const [time, period] = shift.clock_in.split(/\s/)
+                let [hour, minute, seconds] = time.split(':')
+                hour = parseInt(hour)
+                minute = parseInt(minute)
+                seconds = parseInt(seconds)
                 if (period && period.toLowerCase() === 'pm' && hour !== 12) {
-                  hour += 12;
+                  hour += 12
                 } else if (period && period.toLowerCase() === 'am' && hour === 12) {
-                  hour = 0;
+                  hour = 0
                 }
-                let newDateStr = `${month} ${date}, ${year} ${hour}:${minute}:${seconds}`;
-                shift.dateTime = new Date(Date.parse(newDateStr));
-              });
+                const newDateStr = `${month} ${date}, ${year} ${hour}:${minute}:${seconds}`
+                shift.dateTime = new Date(Date.parse(newDateStr))
+              })
               data.shifts.sort((a, b) => {
-                const dateComparison = new Date(b.dateTime) - new Date(a.dateTime);
+                const dateComparison = new Date(b.dateTime) - new Date(a.dateTime)
                 if (dateComparison !== 0) {
-                  return dateComparison;
+                  return dateComparison
                 } else {
-                  const timeA = a.clock_in.split(':');
-                  const timeB = b.clock_in.split(':');
-                  const hourComparison = parseInt(timeB[0]) - parseInt(timeA[0]);
-                  const minuteComparison = parseInt(timeB[1]) - parseInt(timeA[1]);
-                  const secondComparison = parseInt(timeB[2]) - parseInt(timeA[2]);
-    
+                  const timeA = a.clock_in.split(':')
+                  const timeB = b.clock_in.split(':')
+                  const hourComparison = parseInt(timeB[0]) - parseInt(timeA[0])
+                  const minuteComparison = parseInt(timeB[1]) - parseInt(timeA[1])
+                  const secondComparison = parseInt(timeB[2]) - parseInt(timeA[2])
+
                   if (hourComparison !== 0) {
-                    return hourComparison;
+                    return hourComparison
                   } else if (minuteComparison !== 0) {
-                    return minuteComparison;
+                    return minuteComparison
                   } else {
-                    return secondComparison;
+                    return secondComparison
                   }
                 }
-              });
-              this.shifts = data.shifts;
+              })
+              this.shifts = data.shifts
             } else {
-              console.error('Invalid data format in shifts.json');
+              console.error('Invalid data format in shifts.json')
             }
           })
           .catch((error) => {
-            console.error('Failed to fetch shifts data:', error);
-          });
-      };
-    
-      this.fetchJSON = fetchJSON;
+            console.error('Failed to fetch shifts data:', error)
+          })
+      }
+
+      this.fetchJSON = fetchJSON
     },
-    
+
     mounted: function () {
       setTimeout(() => {
-        this.fetchJSON();
-      }, 100);
-      var clickArea = this.$el.querySelector('.clickarea');
-      var overlay = this.$el.querySelector('.overlay');
-      var overlay1 = this.$el.querySelector('.overlay1');
-      var textInput = this.$el.querySelector('.textinput');
-      var button = this.$el.querySelector('.button');
-      var timerElement = this.$el.querySelector("#timer");
-      var timer = this.$el.querySelector('.timer');
-      var timerInterval;
-      var seconds = 0,
-        minutes = 0,
-        hours = 0;
-      var isClicked = false;
-      var currentEarnings = this.$el.querySelector('.currentEarnings');
-      const ONE_MINUTE = 60;
-      const SECONDS_IN_A_DAY = 3600;
-      var clockInTime, clockOutTime;
+        this.fetchJSON()
+      }, 100)
+      const clickArea = this.$el.querySelector('.clickarea')
+      const overlay = this.$el.querySelector('.overlay')
+      const overlay1 = this.$el.querySelector('.overlay1')
+      const textInput = this.$el.querySelector('.textinput')
+      const button = this.$el.querySelector('.button')
+      const timerElement = this.$el.querySelector('#timer')
+      const timer = this.$el.querySelector('.timer')
+      let timerInterval
+      let seconds = 0
+      let minutes = 0
+      let hours = 0
+      let isClicked = false
+      const currentEarnings = this.$el.querySelector('.currentEarnings')
+      const ONE_MINUTE = 60
+      const SECONDS_IN_A_DAY = 3600
+      let clockInTime, clockOutTime
 
-      function startTimer() {
-        timerInterval = setInterval(updateTimer, 1000);
-        timerElement.style.display = "flex";
+      function startTimer () {
+        timerInterval = setInterval(updateTimer, 1000)
+        timerElement.style.display = 'flex'
       }
 
-      function stopTimer() {
-        clearInterval(timerInterval);
+      function stopTimer () {
+        clearInterval(timerInterval)
       }
 
-      function resetTimer() {
-        clearInterval(timerInterval);
-        seconds = 0;
-        minutes = 0;
-        hours = 0;
-        timerElement.textContent = "00:00:00";
+      function resetTimer () {
+        clearInterval(timerInterval)
+        seconds = 0
+        minutes = 0
+        hours = 0
+        timerElement.textContent = '00:00:00'
       }
 
-      function updateEarnings() {
-        var time = timerElement.textContent;
-        var timeArray = time.split(':');
-        var hours = parseInt(timeArray[0]);
-        var minutes = parseInt(timeArray[1]);
-        var seconds = parseInt(timeArray[2]);
-        const payRate = 23.23;
+      function updateEarnings () {
+        const time = timerElement.textContent
+        const timeArray = time.split(':')
+        const hours = parseInt(timeArray[0])
+        const minutes = parseInt(timeArray[1])
+        const seconds = parseInt(timeArray[2])
+        const payRate = 23.23
 
-        var totalSeconds = hours * SECONDS_IN_A_DAY + minutes * ONE_MINUTE + seconds;
-        var earnings = totalSeconds * (payRate / SECONDS_IN_A_DAY);
+        const totalSeconds = hours * SECONDS_IN_A_DAY + minutes * ONE_MINUTE + seconds
+        const earnings = totalSeconds * (payRate / SECONDS_IN_A_DAY)
 
         if (isNaN(earnings)) {
-          currentEarnings.textContent = '$0.00';
+          currentEarnings.textContent = '$0.00'
         } else {
-          currentEarnings.textContent = '$' + earnings.toFixed(2);
+          currentEarnings.textContent = '$' + earnings.toFixed(2)
         }
       }
 
-      earningsInterval = setInterval(updateEarnings, 1000);
+      // eslint-disable-next-line no-undef
+      earningsInterval = setInterval(updateEarnings, 1000)
 
-      function updateTimer() {
-        seconds++;
+      function updateTimer () {
+        seconds++
 
         if (seconds >= 60) {
-          seconds = 0;
-          minutes++;
+          seconds = 0
+          minutes++
 
           if (minutes >= 60) {
-            minutes = 0;
-            hours++;
+            minutes = 0
+            hours++
           }
         }
 
-        var timeString = formatClock(hours) + ":" + formatClock(minutes) + ":" + formatClock(seconds);
-        timerElement.textContent = timeString;
+        const timeString = formatClock(hours) + ':' + formatClock(minutes) + ':' + formatClock(seconds)
+        timerElement.textContent = timeString
       }
 
-      function formatClock(time) {
-        return time < 10 ? "0" + time : time;
+      function formatClock (time) {
+        return time < 10 ? '0' + time : time
       }
 
-      function createRipple(event) {
-        var ripple = document.createElement("span");
+      function createRipple (event) {
+        const ripple = document.createElement('span')
 
-        ripple.className = "ripple";
+        ripple.className = 'ripple'
 
-        var x = event.clientX - event.target.offsetLeft;
-        var y = event.clientY - event.target.offsetTop;
+        const x = event.clientX - event.target.offsetLeft
+        const y = event.clientY - event.target.offsetTop
 
-        ripple.style.left = x + "px";
-        ripple.style.top = y + "px";
+        ripple.style.left = x + 'px'
+        ripple.style.top = y + 'px'
 
-        event.target.appendChild(ripple);
+        event.target.appendChild(ripple)
 
         setTimeout(function () {
-          ripple.remove();
-        }, 800);
+          ripple.remove()
+        }, 800)
       }
 
-      function handleClick(event) {
+      function handleClick (event) {
         if (!isClicked) {
-          clockInTime = new Date(); 
-          overlay.style.display = 'none';
-          overlay1.style.display = 'flex';
-          textInput.style.display = 'block';
-          timer.style.display = 'flex';
-          button.textContent = 'CLOCK OUT';
-          clickArea.style.backgroundColor = '#0da5e0';
-          timerElement.style.fontFamily = "Inconsolata";
-          startTimer();
-          createRipple(event);
+          clockInTime = new Date()
+          overlay.style.display = 'none'
+          overlay1.style.display = 'flex'
+          textInput.style.display = 'block'
+          timer.style.display = 'flex'
+          button.textContent = 'CLOCK OUT'
+          clickArea.style.backgroundColor = '#0da5e0'
+          timerElement.style.fontFamily = 'Inconsolata'
+          startTimer()
+          createRipple(event)
         } else {
-          clockOutTime = new Date(); 
-          overlay.style.display = 'flex';
-          overlay1.style.display = 'none';
+          clockOutTime = new Date()
+          overlay.style.display = 'flex'
+          overlay1.style.display = 'none'
           console.log(clockOutTime)
-          textInput.style.display = 'none';
-          button.textContent = 'CLOCK IN';
+          textInput.style.display = 'none'
+          button.textContent = 'CLOCK IN'
           setTimeout(() => {
-            this.fetchJSON();
-          }, 100);
-          clickArea.style.backgroundColor = '';
-          stopTimer();
-          resetTimer();
-          timerElement.style.display = "none";
-          createRipple(event);
-          var clockinformat = clockInTime.getTime();
-          var clockoutformat = clockOutTime.getTime();
-          console.log("tetstetsttesst " + clockoutformat);
-          var shiftDuration = Math.floor((clockoutformat - clockinformat) / 1000); 
-          console.log(shiftDuration);
-          var shiftCost = (shiftDuration) * (23.23 / SECONDS_IN_A_DAY);
+            this.fetchJSON()
+          }, 100)
+          clickArea.style.backgroundColor = ''
+          stopTimer()
+          resetTimer()
+          timerElement.style.display = 'none'
+          createRipple(event)
+          const clockinformat = clockInTime.getTime()
+          const clockoutformat = clockOutTime.getTime()
+          console.log(clockoutformat)
+          const shiftDuration = Math.floor((clockoutformat - clockinformat) / 1000)
+          console.log(shiftDuration)
+          const shiftCost = (shiftDuration) * (23.23 / SECONDS_IN_A_DAY)
 
-
-          var newShift = {
+          const newShift = {
             date: formatDate(clockInTime),
             clock_in: formatTime(clockInTime),
             clock_out: formatTime(clockOutTime),
             price: '$' + shiftCost.toFixed(2),
-            dateTime: clockInTime,
-          };
+            dateTime: clockInTime
+          }
 
-          saveShiftToFile(newShift);
+          saveShiftToFile(newShift)
         }
-        isClicked = !isClicked;
+        isClicked = !isClicked
       }
 
-      function formatDate(date) {
-        var options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-        return date.toLocaleDateString('en-US', options);
+      function formatDate (date) {
+        const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
+        return date.toLocaleDateString('en-US', options)
       }
 
-
-      function formatTime(date) {
-        return luxon.DateTime.fromJSDate(date).toFormat('HH:mm:ss');
+      function formatTime (date) {
+        // eslint-disable-next-line no-undef
+        return luxon.DateTime.fromJSDate(date).toFormat('HH:mm:ss')
       }
 
-      function saveShiftToFile(shift) {
-        var jsonData = JSON.stringify(shift, null, 2);
+      function saveShiftToFile (shift) {
+        const jsonData = JSON.stringify(shift, null, 2)
 
+        // eslint-disable-next-line no-undef
         $.ajax({
           url: 'save-shifts.php',
           type: 'POST',
           data: jsonData,
           contentType: 'application/json',
           success: function () {
-            console.log('Shift data saved successfully!');
+            console.log('Shift data saved successfully!')
           },
           error: function () {
-            console.error('Failed to save shift data.');
+            console.error('Failed to save shift data.')
           }
-        });
+        })
       }
 
-      clickArea.addEventListener('click', handleClick.bind(this)); 
-      button.addEventListener('click', handleClick.bind(this));
-    },
-  });
-});
+      clickArea.addEventListener('click', handleClick.bind(this))
+      button.addEventListener('click', handleClick.bind(this))
+    }
+  })
+})
